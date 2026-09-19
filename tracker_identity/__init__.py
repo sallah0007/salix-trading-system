0f8873419c0f021655be10925fc932572ddb7173
sallah0007 <sallahuddinrana@gmail.com>
2026-09-19 19:38:45 +1000

    Export Type-1 identity content import boundary

--- tracker_identity/__init__.py
+++ tracker_identity/__init__.py
@@ -9,3 +9,10 @@ from .models import AbsentClaimToken, FeatureIdentity, IdentityLookupResult, Loo
 from .search import identity_lookup
 from .stale_sweep import stale_state_sweep
 from .store import CanonicalIdentityStore
+
+from .importer import (
+    IdentityImportManifest,
+    IdentityImportResult,
+    ImportSourceRef,
+    import_identity_content,
+)
